ff2d83f145623ac9c3672a85c93eda8514293762
nicolewhn <128688171+nicolewhn@users.noreply.github.com>
2023-03-30 18:54:00 +0800

    Create program to extract data from FOMC minutes

---
+++ Data Collection/Minutes.py
@@ -0,0 +1,13 @@
+from FedTools import MonetaryPolicyCommittee
+from FedTools import BeigeBooks
+from FedTools import FederalReserveMins
+from FedTools import MonetaryPolicyCommittee
+from FedTools import BeigeBooks
+from FedTools import FederalReserveMins
+
+#use the find minutes function to return the dataset containing Federal Reserve Minutes since 1993
+dataset = FederalReserveMins().find_minutes()
+dataset.reset_index(inplace=True)
+dataset.rename({"index":"date"},inplace=True)
+# save the dataset into csv format
+dataset.to_csv("../Data/FOMC meeting minutes.csv",index=False)
