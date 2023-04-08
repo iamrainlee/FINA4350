edb7c326b910fbde52098559645deac1ff56aa5f
yzchin <100031934+yzchin@users.noreply.github.com>
2023-04-08 23:04:32 +0800

    Update Statements.py

--- Data Collection/Statements.py
+++ Data Collection/Statements.py
@@ -1,3 +1,4 @@
+# import relevant package and collect dataset:
 from FedTools import MonetaryPolicyCommittee
 dataset = MonetaryPolicyCommittee().find_statements()
 
@@ -9,3 +10,11 @@ import datetime as dt
 # silence warnings:
 import warnings 
 warnings.filterwarnings("ignore")
+
+for i in range(len(dataset)):
+  dataset.iloc[i,0] = dataset.iloc[i,0].replace('\n','. ')
+  dataset.iloc[i,0] = dataset.iloc[i,0].replace('\n',' ')
+  dataset.iloc[i,0] = dataset.iloc[i,0].replace('\r',' ')
+  dataset.iloc[i,0] = dataset.iloc[i,0].replace('\xa0',' ')
+
+dataset.to_csv("FOMCStatement.csv")
