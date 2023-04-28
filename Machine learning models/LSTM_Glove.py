8417e40e4dfb579f616fa8f8e84365ce6b5b0d03
iamrainlee <iamrain@connect.hku.hk>
2023-04-28 16:06:23 +0800

    Updated LSTM

--- Machine learning models/LSTM_Glove.py
+++ Machine learning models/LSTM_Glove.py
@@ -25,6 +25,9 @@ while True:
         maxInt = int(maxInt/10)
 
 def read_glove_vector(glove_vec):
+    """
+    This function is to read the pretrained GloVe and convert into a vector for later transformation
+    """
     with open(glove_vec, 'r', encoding='UTF-8') as f:
         words = set()
         word_to_vec_map = {}
@@ -35,6 +38,10 @@ def read_glove_vector(glove_vec):
     return word_to_vec_map
 
 def rate_predict(input_shape,output_shape):
+    """
+    This is to generate the LSTM model.
+    There are 3 LSTM layers with dropout layer in between, and a fully connected layer at last for classification
+    """
 
     X_indices = Input(input_shape)
 
@@ -57,6 +64,8 @@ def rate_predict(input_shape,output_shape):
     return model
 
 if __name__ == "__main__":
+
+    #This part is for checking the argument input
     if len(sys.argv) != 2:
         print("""Usage of LSTM_Glove.py:
     python3 LSTM_Glove.py [data]
@@ -81,29 +90,39 @@ python3 LSTM_Glove.py [data]
         except:
             print("The path to data is invalid. Please check")
             exit()
+
+    
     print("using data:",sys.argv[1])
+
+    #remove full stop
     data['data'] = data.data.str.replace('.','')
+
+    #separate data into x,y and apply one-hot encoding to y
     X = np.array(data['data'])
     y = pd.get_dummies(data['rate_hike']).values
+
     X_train, X_test,Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_state = 43)
+    
     tokenizer = Tokenizer(num_words=5000)
     tokenizer.fit_on_texts(X_train)
 
     words_to_index = tokenizer.word_index
 
+    #check if Glove library is downloaded
     if not os.path.exists('Library/glove.6B.50d.txt'):
         if not os.path.exists("Library"):
             os.mkdir('Library')
         url = 'https://www.dropbox.com/s/net9dyagiwskupp/glove.6B.50d.txt?dl=1'
         wget.download(url, out='Library/glove.6B.50d.txt')
     
-    word_to_vec_map = read_glove_vector('Library/glove.6B.50d.txt')
+    word_to_vec_map = read_glove_vector('Library/glove.6B.50d.txt') #read the glove vector
 
     maxLen = 300
 
     vocab_len = len(words_to_index)
     embed_vector_len = word_to_vec_map['moon'].shape[0]
 
+    #create embedding of Glove for the data set
     emb_matrix = np.zeros((vocab_len, embed_vector_len))
 
     for word, index in words_to_index.items():
@@ -118,16 +137,18 @@ python3 LSTM_Glove.py [data]
 
     X_train_indices = pad_sequences(X_train_indices, maxlen=maxLen, padding='post')
 
-    model = rate_predict(maxLen,Y_train.shape[1])
+    model = rate_predict(maxLen,Y_train.shape[1]) #create machine learning model
 
-    adam = keras.optimizers.Adam(learning_rate = 0.00005)
+    adam = keras.optimizers.Adam(learning_rate = 0.00005) #add optimizer for model
     model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
 
+    #train model
     model.fit(X_train_indices, Y_train, batch_size=32, epochs=10, validation_split=0.1)
     
     print("Training completed")
     print("Training accuracy:",model.evaluate(X_train_indices, Y_train)[1])
 
+    #transform testing data and evaluate model
     X_test_indices = tokenizer.texts_to_sequences(X_test)
 
     X_test_indices = pad_sequences(X_test_indices, maxlen=maxLen, padding='post')
